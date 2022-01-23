a988f758052b81caaf06188c9702ce70986c2f44
maytham-onaissi <maytham_onaissi@hotmail.com>
2022-01-23 18:48:41 +0300

    Create AudioManager.cs

---
+++ Dagger of the Sands/Assets/Scripts/General/Audio/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance { get; private set; }
+    private AudioSource source;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+
+        //Keep this object even when going to a new scene. 
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        //Destroy duplicate gameObjects.
+        else if (instance != null && instance != this)
+            Destroy(gameObject);
+    }
+
+    public void PlaySound(AudioClip _sound)
+    {
+        source.PlayOneShot(_sound);
+    }
+}
